7e618d0bb6dee591447f6975c10ae286e2494662
tord-s <tord@softeland.name>
2022-03-04 14:40:27 +0100

    Added region to countries

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 import pandas as pd
 
 def read_test():
+    countires_to_regions = pd.read_excel("data/CountriesToRegions.xlsx", sheet_name="Feuil1")
     monthly_returns_dataframe = pd.read_excel("data/monthlyreturns.xlsx", sheet_name="Feuil1")
     firm_names_dataframe = pd.read_excel("data/firm_names.xlsx", sheet_name="Feuil1")
 
-    country_code = "CN"
-    stocks_in_country = firm_names_dataframe.loc[firm_names_dataframe["Country"] == country_code]
+    em_country_codes_raw = list(countires_to_regions.loc[countires_to_regions["Unnamed: 4"] == "{'EM'}"]["function Corresp = CodetoName"])
+    em_country_codes = [code[2:4] for code in em_country_codes_raw]
+
+    stocks_in_country = firm_names_dataframe.loc[firm_names_dataframe["Country"].isin(em_country_codes)]
     ISINs_in_country = stocks_in_country["ISIN"].to_list()
     
     stocks_with_return_dict = {}
@@ -17,7 +20,7 @@ def read_test():
         stocks_with_return_dict[ISIN] = value
 
     sorted_stocks_with_return_list = sorted(stocks_with_return_dict.items(), key=lambda x:x[1])
-    print('5 best stocks in' + country_code + ': ', sorted_stocks_with_return_list[-5:])
+    print('5 best stocks in EM: ', sorted_stocks_with_return_list[-5:])
 
 if __name__ == "__main__":
     read_test()
